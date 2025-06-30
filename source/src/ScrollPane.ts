7c1f55a5b80e7fd23eb3e39db55ef97e06375405
JiaJIa813 <44280310+JiaJIa813@users.noreply.github.com>
2025-06-30 10:05:03 +0800

    Update ScrollPane.ts
    修复GList点击事件异常的问题

--- source/src/ScrollPane.ts
+++ source/src/ScrollPane.ts
@@ -273,7 +273,12 @@ export class ScrollPane extends Component {
                 return this._owner;
         }
 
-        return null;
+         //处理list 点击问题
+        if (pt.x >= this._owner.margin.left && pt.y >= this._owner.margin.top
+            && pt.x < this._owner.margin.left + this._viewSize.x && pt.y < this._owner.margin.top + this._viewSize.y)
+            return this._owner;
+        else
+            return null;
     }
 
     public get owner(): GComponent {
